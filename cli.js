98f0af3c61d00de6072ef407e541ae36eb4424c0
AceArthur10 <115504130+AceArthur10@users.noreply.github.com>
2023-03-22 00:32:47 -0700

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,3 +1,29 @@
-const yargs = require('yargs');
-
+const yargs = require('yargs/yargs');
 const app = require('./app.js');
+
+yargs(process.argv.slice(2))
+  .usage('$0 Usage: <command> [options]')
+  .command(
+    'search [keyword]',
+    'Search for sports teams by keyword',
+    (yargs) => {
+        return(
+            yargs
+                .positional('keyword', {
+                    describe: 'Keyword to search for',
+                    type: 'string',
+                    demandOption: true,
+          })
+          .options('help', {
+            alias: 'h',
+            type: 'boolean',
+            describe: 'Show help',
+          })
+        );
+    },
+    (args) => {
+      app.search(args.keyword);
+    }
+  )
+  .help().argv;
+
