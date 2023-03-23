9ffc0d74373e1b95c340b5ae0c06b837acb45874
AceArthur10 <115504130+AceArthur10@users.noreply.github.com>
2023-03-23 14:49:42 -0700

    Update cli.js

--- cli.js
+++ cli.js
@@ -2,28 +2,31 @@ const yargs = require('yargs/yargs');
 const app = require('./app.js');
 
 yargs(process.argv.slice(2))
-  .usage('$0 Usage: <command> [options]')
-  .command(
-    'search [keyword]',
-    'Search for sports teams by keyword',
-    (yargs) => {
-        return(
-            yargs
-                .positional('keyword', {
-                    describe: 'Keyword to search for',
-                    type: 'string',
-                    demandOption: true,
-          })
-          .options('help', {
-            alias: 'h',
-            type: 'boolean',
-            describe: 'Show help',
-          })
-        );
-    },
-    (args) => {
-      app.search(args.keyword);
-    }
-  )
-  .help().argv;
-
+    .usage('$0: Usage <command> [options]')
+    .command(
+        'search [keyword]',
+        'Search for teams by keyword',
+        (yargs) => {
+            return (    
+                yargs
+                    .positional('keyword', {
+                        describe: 'Keyword to search for',
+                        type: 'string',
+                })  
+                .options('help', {
+                    alias: 'h',
+                    type: 'boolean',
+                    describe: 'Show help',
+                  })
+            );
+        },
+        (args) => {
+            if (args.keyword) {
+                app.displayResults(args.keyword);
+            } else {
+                console.log('Enter a keyword to search for.');
+            }
+        },
+    )
+    .help()
+    .argv;
